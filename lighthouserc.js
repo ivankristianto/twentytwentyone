91be34445fe1fb54a82714aba063dad7bb4b8c8a
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-10-27 12:57:29 +0700

    Only run on PR

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,18 @@ module.exports = {
       target: 'lhci',
     },
     assert: {
-      "budgetsFile": "./budget.json"
+    	// "budgetsFile": "./budget.json"
+      preset: 'lighthouse:recommended',
+      "assertions": {
+        "dom-size": ["error", {"maxNumericValue": 3000}],
+        "first-contentful-paint": ["warn", {"maxNumericValue": 3000}],
+        "viewport": "error",
+        "installable-manifest": "off",
+        "offline-start-url": "off",
+        "service-worker": "off",
+        "splash-screen": "off",
+        "works-offline": "off"
+      }
     },
   },
 };
