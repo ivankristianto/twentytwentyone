0b2394015519067877f64190f258f8d238a65509
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-10-27 12:52:50 +0700

    Rerun assertions with budget.json

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,17 +4,7 @@ module.exports = {
       target: 'lhci',
     },
     assert: {
-      preset: 'lighthouse:recommended',
-      "assertions": {
-        "dom-size": ["error", {"maxNumericValue": 3000}],
-        "first-contentful-paint": ["warn", {"maxNumericValue": 3000}],
-        "viewport": "error",
-        "installable-manifest": "off",
-        "offline-start-url": "off",
-        "service-worker": "off",
-        "splash-screen": "off",
-        "works-offline": "off"
-      }
+      "budgetsFile": "./budget.json"
     },
   },
 };
